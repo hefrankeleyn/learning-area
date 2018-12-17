dd24822b347c8a1b09d78eec02d51a95e7c1dfdf
lifei <hefrankeleyn@gmail.com>
2018-12-17 14:06:57 +0800

    为form-index.php添加表单验证

--- 003PHP/form-index.php
+++ 003PHP/form-index.php
@@ -10,6 +10,8 @@
     <?php
         // 定义变量，并设置为空值
         $username = $email = $website = $comment = $gender = "";
+        // 添加一组变量，用于存放每个字段的错误消息
+        $usernameError = $emailError = $websiteError = $commentError = $genderError = "";
         // 对用户输入的数据进行处理
         function test_input($data){
             # 去除数据两边的空格
@@ -22,31 +24,66 @@
         }
         // 
         if ($_SERVER["REQUEST_METHOD"] === "POST"){
-            $username = test_input($_POST["username"]);
-            $email = test_input($_POST["email"]);
-            $website = test_input($_POST["website"]);
-            $comment = test_input($_POST["comment"]);
-            $gender = test_input($_POST["gender"]);
+            # 判断username是否为空
+            if (empty($_POST["username"])){
+                $usernameError = "Username is required.";
+            }else{
+                $username = test_input($_POST["username"]);
+                // 检验名字是否包含字母空格
+                if(!preg_match("/^[a-zA-Z ]*$/",$username)){
+                    $usernameError = "Only letters and white space allowed.";
+                }
+            }
+            if (empty($_POST["email"])){
+                $emailError = "Email is required.";
+            }else{
+                $email = test_input($_POST["email"]);
+                // 检查邮箱是否有效
+                if (!preg_match("/([\w\-]+\@[\w\-]+\.[\w\-]+)/",$email)){
+                    $emailError = "Invalid email format.";
+                }
+            }
+            if (empty($_POST["website"])){
+                $websiteError = "";
+            }else{
+                $website = test_input($_POST["website"]);
+                // 检查URL地址语言是否有效（此URL中同样允许URL中的下划线）
+                if(!preg_match("/\b(?:(?:https?|ftp):\/\/|www\.)[-a-z0-9+&@#\/%?=~_|!:,.;]*[-a-z0-9+&@#\/%=~_|]/i",$website)){
+                    $websiteError = "Invalid URL";
+                }
+            }
+            if (empty($_POST["comment"])){
+                $commentError = "";
+            }else{
+                $comment = test_input($_POST["comment"]);
+            }
+            if (empty($_POST["gender"])){
+                $genderError = "Gender is required.";
+            }else{
+                $gender = test_input($_POST["gender"]);
+            }
+
         }
         
     ?>
     <form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>" method="POST">
         <p>
-            姓名：<input name="username" type="text">
+            姓名：<input name="username" type="text" value="<?php echo $username;?>"><span class="error">*<?php echo $usernameError?></span>
         </p>
         <p>
-            邮箱：<input name="email" type="text">
+            邮箱：<input name="email" type="text" value="<?php echo $email;?>"><span class="error">*<?php echo $emailError?></span>
         </p>
         <p>
-            网址：<input name="website" type="text">
+            网址：<input name="website" type="text" value="<?php echo $website;?>"><span class="error"><?php echo $websiteError?></span>
         </p>
         <p>
-            评论：<textarea name="comment" id="" cols="30" rows="10"></textarea>
+            评论：<textarea name="comment" id="" cols="30" rows="10"><?php echo $comment;?></textarea><span class="error"><?php echo $commentError?></span>
         </p>
         <p>
             <span>性别：</span>
-            <span><input type="radio" name="gender" value="female"> FeMale </span>
-            <span><input type="radio" name="gender" value="male"> Male </span>
+            <span><input type="radio" name="gender" <?php if(isset($gender) && $gender == "female") echo "checked";?> value="female" > FeMale </span>
+            <span><input type="radio" name="gender" value="male" <?php if(isset($gender) && $gender=="male") echo "checked";?>> Male </span> 
+            <span class="error">*<?php echo $genderError?></span>
         </p>
         <p><button type="submit">submit</button></p>
     </form>
