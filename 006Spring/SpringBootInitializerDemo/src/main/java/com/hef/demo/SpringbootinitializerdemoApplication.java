01d9f66fc4a3ae3c8432439c4b686644507876ab
Li Fei <hefrankeleyn@gamil.com>
2019-05-06 13:44:14 +0800

    Spring Boot initializer，初始化一个个项目

---
+++ 006Spring/SpringBootInitializerDemo/src/main/java/com/hef/demo/SpringbootinitializerdemoApplication.java
@@ -0,0 +1,13 @@
+package com.hef.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootinitializerdemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootinitializerdemoApplication.class, args);
+    }
+
+}
