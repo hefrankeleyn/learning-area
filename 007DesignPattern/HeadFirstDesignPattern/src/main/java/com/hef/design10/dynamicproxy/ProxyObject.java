9bc8ab2f149561af51d5ef34dc82213bb15d7df3
Li Fei <hefrankeleyn@gamil.com>
2019-06-14 13:23:57 +0800

    动态代理-添加注释

--- 007DesignPattern/HeadFirstDesignPattern/src/main/java/com/hef/design10/dynamicproxy/ProxyObject.java
+++ 007DesignPattern/HeadFirstDesignPattern/src/main/java/com/hef/design10/dynamicproxy/ProxyObject.java
@@ -4,31 +4,44 @@ import java.lang.reflect.InvocationHandler;
 import java.lang.reflect.Proxy;
 
 /**
+ * 写代码创建动态代理
+ *
  * @Date 2019-06-13
  * @Author lifei
  */
 public class ProxyObject {
 
-    public PersonBean getOwnerProxy(PersonBean person){
+    /**
+     * 创建 OwnerInvocationHandler 的代理
+     *
+     * @param person
+     * @return
+     */
+    public PersonBean getOwnerProxy(PersonBean person) {
         return (PersonBean) Proxy.newProxyInstance(
                 person.getClass().getClassLoader(),
                 person.getClass().getInterfaces(),
                 new OwnerInvocationHandler(person));
     }
 
-    public PersonBean getNonOwnerProxy(PersonBean person){
-        return (PersonBean) Proxy.newProxyInstance(person.getClass().getClassLoader(),
-                person.getClass().getInterfaces(),new NonOwnerInvocationHandler(person));
+    /**
+     * 创建 NonOwnerInvocationHandler 的代理
+     *
+     * @param person
+     * @return
+     */
+    public PersonBean getNonOwnerProxy(PersonBean person) {
+        return (PersonBean) Proxy.newProxyInstance(
+                person.getClass().getClassLoader(),
+                person.getClass().getInterfaces(),
+                new NonOwnerInvocationHandler(person));
     }
 
 
-    public PersonBean getProxy(PersonBean person, InvocationHandler handler){
+    public PersonBean getProxy(PersonBean person, InvocationHandler handler) {
         return (PersonBean) Proxy.newProxyInstance(
                 person.getClass().getClassLoader(),
                 person.getClass().getInterfaces(),
                 handler);
     }
-
-
-
 }
