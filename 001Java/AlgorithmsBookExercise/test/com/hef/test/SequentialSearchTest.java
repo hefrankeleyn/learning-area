b06c4b8d2646b420e4984042e806882aa0ecd0e3
Li Fei <hefrankeleyn@gamil.com>
2019-10-20 14:58:41 +0800

    Binary search and sequential search test

--- 001Java/AlgorithmsBookExercise/test/com/hef/test/SequentialSearchTest.java
+++ 001Java/AlgorithmsBookExercise/test/com/hef/test/SequentialSearchTest.java
@@ -1,5 +1,6 @@
 package com.hef.test;
 
+import com.hef.algorithms.fundamentals.symboltable.BinarySearchST;
 import com.hef.algorithms.fundamentals.symboltable.SequentialSearchST;
 import org.junit.Test;
 
@@ -13,7 +14,16 @@ public class SequentialSearchTest {
     public void test01(){
         SequentialSearchST<String, String> symbolTable = new SequentialSearchST<>();
         symbolTable.put("aa", "bb");
-
         System.out.println(symbolTable.get("aa"));
+    }
+
+    @Test
+    public void binarySearchTest(){
+        BinarySearchST<Integer, String> bst = new BinarySearchST<>(10);
+        bst.put(2, "bb");
+        bst.put(6, "cc");
+        System.out.println(bst.get(7));
+        System.out.println(bst.get(2));
+        System.out.println(bst.get(6));
     }
 }
